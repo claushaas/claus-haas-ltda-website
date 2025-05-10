dc4107a7188e0a05ad431aceb7cbc8f8731cd5aa
Claus Haas <claus.haas@me.com>
2025-05-09 22:33:29 -0300

    fix: update Face component styles and add text shadows to headings

--- app/routes/home.tsx
+++ app/routes/home.tsx
@@ -1,5 +1,4 @@
 import Face from '../../public/images/face.svg?react';
-import type { Route } from './+types/home';
 
 export function meta() {
 	return [
@@ -13,12 +12,16 @@ export default function Home() {
 		<main className="m-auto max-w-4xl px-4 py-8">
 			<section className="flex flex-wrap gap-4 md:flex-nowrap">
 				<div className="flex w-full justify-center md:justify-start">
-					<Face className="max-h-72 fill-sky-11 md:max-h-96" />
+					<Face className="max-h-72 fill-sky-12 stroke-2 stroke-sky-11 md:max-h-96 dark:fill-skydark-11 dark:stroke-skydark-10" />
 				</div>
 				<div className="flex w-full flex-col justify-between">
 					<div className="flex flex-col items-end">
-						<h1 className="text-9xl">Claus</h1>
-						<h1 className="text-9xl">Haas</h1>
+						<h1 className="text-9xl text-shadow-md text-shadow-sky-12 dark:text-shadow-skydark-12">
+							Claus
+						</h1>
+						<h1 className="text-9xl text-shadow-md text-shadow-sky-12 dark:text-shadow-skydark-12">
+							Haas
+						</h1>
 					</div>
 					<div className="flex flex-col items-end">
 						<p className="text-2xl">Fullstack Developer</p>
