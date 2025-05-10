f2a35a4204edfa4eb1ea9aefa0a11b25a9172a78
Claus Haas <claus.haas@me.com>
2025-05-09 22:10:43 -0300

    Add Face SVG and update Home component layout
    - Imported Face SVG as a React component for use in the Home page.
    - Updated the Home component to include a new layout with the Face SVG, a title, and a list of skills.
    - Removed the previous loader function and its usage in the Home component.

--- app/routes/home.tsx
+++ app/routes/home.tsx
@@ -1,3 +1,4 @@
+import Face from '../../public/images/face.svg?react';
 import type { Route } from './+types/home';
 
 export function meta() {
@@ -7,14 +8,25 @@ export function meta() {
 	];
 }
 
-export function loader({ context }: Route.LoaderArgs) {
-	return { message: context.cloudflare.env.VALUE_FROM_CLOUDFLARE };
-}
-
-export default function Home({ loaderData }: Route.ComponentProps) {
+export default function Home() {
 	return (
-		<main>
-			<h1 className="font-sans">Claus Haas</h1>
+		<main className="m-auto max-w-4xl px-4 py-8">
+			<section className="flex flex-wrap gap-4 md:flex-nowrap">
+				<div className="flex w-full justify-center md:justify-start">
+					<Face className="max-h-72 fill-sky-11 md:max-h-96" />
+				</div>
+				<div className="flex w-full flex-col justify-between">
+					<div className="flex flex-col items-end">
+						<h1 className="text-9xl">Claus</h1>
+						<h1 className="text-9xl">Haas</h1>
+					</div>
+					<div className="flex flex-col items-end">
+						<p className="text-2xl">Fullstack Developer</p>
+						<p className="text-2xl">Automation Expert</p>
+						<p className="text-2xl">CRM Specialist</p>
+					</div>
+				</div>
+			</section>
 		</main>
 	);
 }
