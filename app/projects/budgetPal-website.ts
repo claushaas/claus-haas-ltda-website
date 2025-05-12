c040c9ca4fa7fe8c5d6c0270023ae61fd005039f
Claus Haas <claus.haas@me.com>
2025-05-12 18:02:02 -0300

    feat: add budgetPal project details to budgetPal-website.ts

---
+++ app/projects/budgetPal-website.ts
@@ -0,0 +1,16 @@
+export const project = {
+	description: 'Este site.',
+	highlights: ['SSR', 'Cloudflare Workers'],
+	link: 'https://budgetpalapp.com',
+	technologies: [
+		'React',
+		'React Native',
+		'React Router',
+		'Typescript',
+		'Biome',
+		'Radix Colors',
+		'Motion',
+		'Cloudflare Workers',
+	],
+	title: 'busgetPal Website',
+};
