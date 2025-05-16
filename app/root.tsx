7ee4c9b4649586f94f708980eb2999d22376885b
Claus Haas <claus.haas@me.com>
2025-05-15 22:18:01 -0300

    fix: add background color to body in Layout component

--- app/root.tsx
+++ app/root.tsx
@@ -43,7 +43,7 @@ export function Layout({ children }: { children: React.ReactNode }) {
 				<Meta />
 				<Links />
 			</head>
-			<body className="m-auto h-fit max-w-4xl space-y-16 px-4">
+			<body className="m-auto h-fit max-w-4xl space-y-16 bg-slate-1 px-4 dark:bg-slatedark-1">
 				{children}
 				<ScrollRestoration />
 				{isBot ? null : <Scripts />}
