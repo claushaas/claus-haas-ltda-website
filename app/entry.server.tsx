0a1867f58264553b3eaf65112ec64f16941c922a
Claus Haas <claus.haas@me.com>
2025-05-20 16:53:05 -0300

    refactor: switch from renderToString to renderToReadableStream for improved streaming support

--- app/entry.server.tsx
+++ app/entry.server.tsx
@@ -1,29 +1,45 @@
 import { isbot } from 'isbot';
-import { renderToString } from 'react-dom/server';
-import type { EntryContext } from 'react-router';
+import { renderToReadableStream } from 'react-dom/server';
+import type { AppLoadContext, EntryContext } from 'react-router';
 import { ServerRouter } from 'react-router';
 import { IsBotProvider } from './hooks/use-is-bot';
-
-export const streamTimeout = 5_000;
 
 export default async function handleRequest(
 	request: Request,
 	responseStatusCode: number,
 	responseHeaders: Headers,
-	entryContext: EntryContext,
+	routerContext: EntryContext,
+	_loadContext: AppLoadContext,
 ) {
-	let userAgent = request.headers.get('user-agent');
-	const isBotUser = isbot(userAgent ?? '');
+	let shellRendered = false;
+	const userAgent = request.headers.get('user-agent');
 
-	const markup = renderToString(
-		<IsBotProvider isBot={isBotUser}>
-			<ServerRouter context={entryContext} url={request.url} />
+	const body = await renderToReadableStream(
+		<IsBotProvider isBot={isbot(userAgent ?? '')}>
+			<ServerRouter context={routerContext} url={request.url} />
 		</IsBotProvider>,
+		{
+			onError(error: unknown) {
+				responseStatusCode = 500;
+				// Log streaming rendering errors from inside the shell.  Don't log
+				// errors encountered during initial shell rendering since they'll
+				// reject and get logged in handleDocumentRequest.
+				if (shellRendered) {
+					console.error(error);
+				}
+			},
+		},
 	);
+	shellRendered = true;
+
+	// Ensure requests from bots and SPA Mode renders wait for all content to load before responding
+	// https://react.dev/reference/react-dom/server/renderToPipeableStream#waiting-for-all-content-to-load-for-crawlers-and-static-generation
+	if ((userAgent && isbot(userAgent)) || routerContext.isSpaMode) {
+		await body.allReady;
+	}
 
 	responseHeaders.set('Content-Type', 'text/html');
-
-	return new Response(markup, {
+	return new Response(body, {
 		headers: responseHeaders,
 		status: responseStatusCode,
 	});
