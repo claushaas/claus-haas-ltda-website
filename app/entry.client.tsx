2f52e97a8dc8cdfb65c74af889cec1267c721dce
Claus Haas <claus.haas@me.com>
2025-05-19 22:14:59 -0300

    Adds i18next internationalization support
    Wraps the application in an I18nextProvider to provide
    internationalization support.
    
    Initializes i18next to load translations.

--- app/entry.client.tsx
+++ app/entry.client.tsx
@@ -1,12 +1,19 @@
+import i18n from 'i18next';
 import { StrictMode, startTransition } from 'react';
 import { hydrateRoot } from 'react-dom/client';
+import { I18nextProvider } from 'react-i18next';
 import { HydratedRouter } from 'react-router/dom';
+import { initI18Next } from './i18n/i18n';
 
-startTransition(() => {
+startTransition(async () => {
+	await initI18Next(i18n);
+
 	hydrateRoot(
 		document,
 		<StrictMode>
-			<HydratedRouter />
+			<I18nextProvider i18n={i18n}>
+				<HydratedRouter />
+			</I18nextProvider>
 		</StrictMode>,
 	);
 });
